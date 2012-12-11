7ae81d3cfd67a435a89a39fc74436831750a1a95
Suslik73 <zrr-rustam@mail.ru>
2012-12-12 01:14:37 +0400

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab3spo::Application.config.secret_token = '0af231b0d164312291f6ef249879cfc10e4ecfda21bd71bbb70f34ae67ab56be98e1c3d6cb0860d1322b7ac6243b01e245ac5c733e1472c9a1ee88519b71f5a7'
