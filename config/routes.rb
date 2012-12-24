55c38f22726de3c3fb94f9ac09981a7819da10a0
Suslik73 <zrr-rustam@mail.ru>
2012-12-24 21:38:31 +0400

    state_machine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Lab3spo::Application.routes.draw do
+  resources :books
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,6 +53,8 @@ Lab3spo::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+
+  root :to => 'books#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
